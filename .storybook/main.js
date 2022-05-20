729b4000d8b41125b635f63052561e367a2ba3f7
Daniel Stoian <daniel.stoian@lgepartner.com>
2022-05-20 14:06:50 +0300

    commit for 6.4.22

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,8 @@ module.exports = {
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-links/register",
+    "@storybook/addon-essentials/register"
   ],
   "framework": "@storybook/react"
 }
